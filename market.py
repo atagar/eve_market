6b2957db4df5d8d08a761b5d41cb922e5f0a93fc
Damian Johnson <atagar@torproject.org>
2026-01-01 17:35:42 -0800

    List all LP store skill books
    Most skill books come from NPC sellers. However, three social skills and the
    'Specialization' skills come from the LP store so their prices vary more...
    
      https://wiki.eveuniversity.org/Loyalty_Points#Skillbooks

--- market.py
+++ market.py
@@ -12,8 +12,8 @@ import urllib.request
 API_URL = 'https://triff.tools/api/prices/station/?station_id=%i&type_ids=%s'
 STATIC_TYPES = 'eve-online-static-data-3142455-jsonl/types.jsonl'
 
-DIV = '+------------------------------+--------------------+--------------------+--------------------+------------------------------+------------------------------+'
-LINE = '| {:<28} | {:>18} | {:>18} | {:>18} | {:>28} | {:>28} |'
+DIV = '+----------------------------------------+--------------------+--------------------+--------------------+------------------------------+------------------------------+'
+LINE = '| {:<38} | {:>18} | {:>18} | {:>18} | {:>28} | {:>28} |'
 
 # from https://www.adam4eve.eu/info_stations.php
 #
@@ -38,10 +38,67 @@ ORES = collections.OrderedDict((
 ))
 
 
+# Skills from the LP stores (hence no direct NPC sellers).
+
 SKILLS = collections.OrderedDict((
   ('Mining Connections', 3893),
   ('Distribution Connections', 3894),
   ('Security Connections', 3895),
+
+  ('Small Railgun Specialization', 11082),
+  ('Small Beam Laser Specialization', 11083),
+  ('Small Autocannon Specialization', 11084),
+  ('Small Artillery Specialization', 12201),
+
+  ('Medium Artillery Specialization', 12202),
+  ('Large Artillery Specialization', 12203),
+  ('Medium Beam Laser Specialization', 12204),
+  ('Large Beam Laser Specialization', 12205),
+  ('Medium Railgun Specialization', 12206),
+  ('Large Railgun Specialization', 12207),
+  ('Medium Autocannon Specialization', 12208),
+  ('Large Autocannon Specialization', 12209),
+  ('Small Blaster Specialization', 12210),
+  ('Medium Blaster Specialization', 12211),
+  ('Large Blaster Specialization', 12212),
+  ('Small Pulse Laser Specialization', 12213),
+  ('Medium Pulse Laser Specialization', 12214),
+  ('Large Pulse Laser Specialization', 12215),
+
+  ('Amarr Drone Specialization', 12484),
+  ('Minmatar Drone Specialization', 12485),
+  ('Gallente Drone Specialization', 12486),
+  ('Caldari Drone Specialization', 12487),
+
+  ('Rocket Specialization', 20209),
+  ('Light Missile Specialization', 20210),
+  ('Heavy Missile Specialization', 20211),
+  ('Cruise Missile Specialization', 20212),
+  ('Torpedo Specialization', 20213),
+  ('Mining Drone Specialization', 22541),
+  ('Heavy Assault Missile Specialization', 25718),
+
+  ('Capital Autocannon Specialization', 41403),
+  ('Capital Artillery Specialization', 41404),
+  ('Capital Blaster Specialization', 41405),
+  ('Capital Railgun Specialization', 41406),
+  ('Capital Pulse Laser Specialization', 41407),
+  ('Capital Beam Laser Specialization', 41408),
+  ('XL Torpedo Specialization', 41409),
+  ('XL Cruise Missile Specialization', 41410),
+
+  ('Ice Harvesting Drone Specialization', 43703),
+
+  ('Small Disintegrator Specialization', 47873),
+  ('Medium Disintegrator Specialization', 47874),
+  ('Large Disintegrator Specialization', 47875),
+
+  ('Small Vorton Specialization', 54827),
+  ('Medium Vorton Specialization', 54828),
+  ('Large Vorton Specialization', 54829),
+
+  ('Salvage Drone Specialization', 57164),
+  ('Mutated Drone Specialization', 60515),
 ))
 
 
@@ -71,7 +128,7 @@ def resolve(item):
 
 def get_prices(station, items):
   url = API_URL % (station, ','.join(map(str, items)))
-  cache_path = os.path.join('cache', '{}:{}'.format(station, '-'.join(map(str, items))))
+  cache_path = os.path.join('cache', '{}:{}'.format(station, hash('-'.join(map(str, items)))))
 
   if os.path.exists(cache_path):
     with open(cache_path) as cache_file:
